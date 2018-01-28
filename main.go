d86b6393368aec5b8eeae2367b3fb80919bd9e83
sw33tr0ll <logan.evans96@gmail.com>
2018-01-28 12:53:47 -0700

    Added server and client functions

--- main.go
+++ main.go
@@ -3,29 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 )
 
 func main() {
 
 	serverMode := flag.Bool("server", false, "Starts ContRoll in server mode")
 	port := flag.Int("port", 0, "The TCP port to connect on")
-
+	host := flag.String("host", "127.0.0.1", "IP of the host you want to connect to")
 	flag.Parse()
-
-	if *serverMode {
-		contRollServer()
-	}
 
 	if *port < 1 || *port > 65535 {
 		fmt.Print("Please specify a port to connect on with the --port=# flag")
-		os.Exit(1)
 	}
 
-	fmt.Print("Connecting on port ", *port, "...")
-	fmt.Print("This code doesn't actually do anything\nExiting...\n")
+	if *serverMode {
+		contRollServer(*port)
+	} else {
+		contRollClient(*host, *port)
+	}
+
+	fmt.Print("\n\nThis code doesn't actually do anything\nExiting...\n")
 }
 
-func contRollServer() {
-	fmt.Print("ContRoll started in server mode...\n")
+func contRollServer(port int) {
+	fmt.Print("ContRoll started in server mode...\n\n")
+	fmt.Print("Waiting for connections on port ", port, "...")
+}
+
+func contRollClient(host string, port int) {
+	fmt.Print("ContRoll started in client mode...\n\n")
+	fmt.Print("Connecting to ", host, " on port ", port, "...")
 }
